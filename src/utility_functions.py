3541cd5568735b63cb5b85dcc85e50bd8e83109e
lagillenwater <lagillenwater@gmail.com>
2024-05-30 13:23:52 -0600

    created function to map list of entrez_id's to gene symbols

--- src/utility_functions.py
+++ src/utility_functions.py
@@ -3,8 +3,8 @@
 # libraries
 import argparse
 import networkx as nx
-from PyEntrezId import Conversion
-
+import pandas as pd
+import warnings
 ## arguments.
 ## 2024-05-28 - include edgelist file and source.
 
@@ -13,6 +13,7 @@ def define_arguments():
 
     parser.add_argument( '-e', dest = "edgelist_file",help="Edgelist file. The format may be dependent upon the network. Default is [entrez gene id 1][entrez gene id 2][posterior prob.] based on the HumanBase full network format")
     parser.add_argument( '-s', dest = "source",default = "HumanBase", help ="Source of the edgelist. May be 'HumanBase' (default).")
+    parser.add_argument( '-m', dest = "hgnc_file", help ="HGNC mapping file for converting entrez IDs to HGNC symbols")
     
     return parser
 
@@ -24,8 +25,9 @@ def generate_arguments():
     args = parser.parse_args()
     edgelist = args.edgelist_file
     source = args.source
+    hgnc_file = args.hgnc_file
 
-    return edgelist,source
+    return edgelist,source,hgnc_file
 
 
 # create graph from edgelist file edgelist
@@ -43,12 +45,19 @@ def three_edges(G):
     for edge in first_three_edges:
         print(edge)
 
+## convert a list of entrez ids to gene symbols
+def entrez_to_hgnc(entrez_ids,hgnc_file):
+    hgnc = pd.read_csv(hgnc_file, sep = "\t", low_memory = False)
+    hgnc['entrez_id'] = hgnc['entrez_id'].astype(pd.Int64Dtype()).astype(str)
+    # filter by list of entrez_ids
+    result_df = hgnc[hgnc['entrez_id'].isin(entrez_ids)]
+    # Sort the DataFrame based on the 'order' column
+    warnings.simplefilter("ignore") ## See the caveats in the documentation: https://pandas.pydata.org/pandas-docs/stable/user_guide/indexing.html#returning-a-view-versus-a-copy  result_df['entrez_id'] = result_df['entrez_id'].map({id: i for i, id in enumerate(entrez_ids)})/Users/lucas/Library/CloudStorage/OneDrive-TheUniversityofColoradoDenver/Projects/Net21/src/utility_functions.py:55: SettingWithCopyWarning: A value is trying to be set on a copy of a slice from a DataFrame
+    result_df['entrez_id'] = result_df['entrez_id'].map({id: i for i, id in enumerate(entrez_ids)})
+    warnings.simplefilter("ignore") ## See the caveats in the documentation: https://pandas.pydata.org/pandas-docs/stable/user_guide/indexing.html#returning-a-view-versus-a-cop
+    result_df.sort_values(by='entrez_id', inplace=True)
+    hgnc_symbols = result_df['symbol'].tolist()
+    return hgnc_symbols
+    
 
-
-
-def entrez_to_hgnc(entrez_id):
-    Id = Conversion('dummyemail@dummybunny.info')
-    hgnc_symbol = Id.convert_entrez_to_hgnc(entrez_id)
-    return(hgnc_symbol)
-
-
+    
--- src/wrapper_script.py
+++ src/wrapper_script.py
@@ -5,14 +5,14 @@ from RWR import RWR
 # main function
 def main():
 
-    edgelist,source = generate_arguments()
-    print("###### Creating graph from edgelist: " + edgelist + " #######")
-    G = read_edgelist(edgelist,source)
-    print('###### Running random walk with restart (aka, PageRank) ######')
-    pr = RWR(G)
-    print('###### top RWR scores:  ######')
-    print(dict(list(pr.items())[:10]))
-
+    edgelist,source,hgnc_file = generate_arguments()
+    # print("###### Creating graph from edgelist: " + edgelist + " #######")
+    # G = read_edgelist(edgelist,source)
+    # print('###### Running random walk with restart (aka, PageRank) ######')
+    # pr = RWR(G)
+    # print('###### top RWR scores:  ######')
+    # print(dict(list(pr.items())[:10]))
+    entrez_to_hgnc(['100', '1000', '1'],hgnc_file) 
     
 if __name__ == '__main__':
     main()
